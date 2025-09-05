776a99229905cf9b6c512b814a247d2db47bc3d4
Quentin Agosta <quentin.agosta.agora2024@gmail.com>
2025-09-05 11:52:02 +0200

    Add Config

---
+++ MVC/Config/database.php
@@ -0,0 +1,8 @@
+<?php
+    define("BD_DRIVER", "mysql");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_DATABASE", "mvcsample");
+    define("DB_CHARSET", "utf8");
+?>
---
+++ MVC/Config/global.php
@@ -0,0 +1,4 @@
+<?php
+    define("CONTROLLER_DEFAULT", "articles");
+    define("ACTION_DEFAULT", "index"); 
+?>
