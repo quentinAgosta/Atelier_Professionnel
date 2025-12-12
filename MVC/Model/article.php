d8d544d5e6e34544558713d105adb4e52fe51a9c
Quentin Agosta <quentin.agosta.agora2024@gmail.com>
2025-12-12 11:41:23 +0100

    Rangement Git

---
+++ MVC/Model/article.php
@@ -0,0 +1,15 @@
+<?php
+    class Article{
+        private $table="article";
+        private $connexion;
+
+        private $art_id;
+        private $art_nom;
+        private $art_prix;
+        private $art_poids;
+
+        public function __construct($connexion){
+            
+        }
+    }
+?>
